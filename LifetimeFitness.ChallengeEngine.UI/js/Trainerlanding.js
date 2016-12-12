4811b9fbda4ab38017a2db50535615e942c2ba4b
ltfuser01 <ltfuser01@AZID1WLTF01>
2016-12-12 08:42:55 +0000

    post 3

--- LifetimeFitness.ChallengeEngine.UI/js/Trainerlanding.js
+++ LifetimeFitness.ChallengeEngine.UI/js/Trainerlanding.js
@@ -1,32 +1,12 @@
 ﻿var app = angular.module('ltfAppTrainer', []);
 
-app.controller('CategoryCtrl', function ($scope, $http) {
-    $scope.Categorylist = [];
-    $http({
-        method: 'GET',
-        url: 'http://localhost:56507/api/Category/GetCategory'
-    }).success(function (result) {
-        console.log(result);
-        $scope.Categorylist = result;
-    });
-});
-
-app.controller('ChallengeCtrl', function ($scope, $http) {
-    $scope.Challengelist = [];
-    $http({
-        method: 'GET',
-        url: 'http://localhost:56507/api/Challenge/GetChallenges'
-    }).success(function (result) {
-        console.log(result);
-        $scope.Challengelist = result;
-    });
-});
-
 app.controller('myenroll', function ($scope, $http) {
     $scope.eCategorylist = [];
     $scope.eChallengelist = [];
     $scope.eUserlist = [];
-    $scope.eClublist = 0;
+    $scope.eClublist = [];
+
+
     $http.get("http://localhost:56507/api/Club/GetClub").then(function (responses) {
         console.log(responses);
         $scope.eClublist = responses.data;
@@ -35,28 +15,28 @@ app.controller('myenroll', function ($scope, $http) {
     $http.get("http://localhost:56507/api/Category/GetCategory").then(function (responses) {
         console.log(responses);
         $scope.eCategorylist = responses.data;
-        $scope.myCategoryValue = $scope.myCategoryValue;
+        $scope.myCategoryValue = $scope.eCategorylist;
     });
     $http.get("http://localhost:56507/api/Challenge/GetChallenges").then(function (responses) {
         console.log(responses);
         $scope.eChallengelist = responses.data;
-        $scope.myChallengeValue = $scope.myChallengeValue;
+        $scope.myChallengeValue = $scope.eChallengelist;
     });
     $http.get("http://localhost:56507/api/User/GetUsers").then(function (responses) {
         console.log(responses);
         $scope.eUserlist = responses.data;
         $scope.myUserValue = $scope.eUserlist[0];
     });
-    $scope.getCategory = function (myCategoryValue) {
-        console.log(myCategoryValue);
-        console.log(myeCategoryValue);
-        $scope.myeCategoryValue = myCategoryValue;
-    };
-    $scope.getChallenge = function (myChallengeValue) {
-        console.log(myCategoryValue);
-        console.log(myeChallengeValue);
-        $scope.myeChallengeValue = myChallengeValue;
-    };
+    //$scope.getCategory = function (myCategoryValue) {
+    //    console.log(myCategoryValue);
+    //    console.log(myeCategoryValue);
+    //    $scope.myeCategoryValue = myCategoryValue;
+    //};
+    //$scope.getChallenge = function (myChallengeValue) {
+    //    console.log(myCategoryValue);
+    //    console.log(myeChallengeValue);
+    //    $scope.myeChallengeValue = myChallengeValue;
+    //};
     $scope.enrollUser = function (myCategoryValue, myChallengeValue, myUserValue) {
         //var data = {
         //    category: myCategoryValue,
