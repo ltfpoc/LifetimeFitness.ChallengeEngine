7131af76a9157135b2138317821fc96f118149b4
sunitamishra12 <sunitamishra1@gmail.com>
2016-12-12 08:13:58 +0000

    Updated changes.

---
+++ LifetimeFitness.ChallengeEngine.Business/BusinessLayer/ChallengesBL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifetimeFitness.ChallengeEngine.Business.BusinessLayer
+{
+    class ChallengesBL
+    {
+        private readonly ChallengeProvider _challengestore;
+        private readonly ClubProvider _clubstore;
+
+        public ChallengesBL()
+        {
+            _challengestore = new ChallengeProvider();
+            _clubstore = new ClubProvider();
+        }
+
+
+    }
+}
--- LifetimeFitness.ChallengeEngine.UI/js/Trainerlanding.js
+++ LifetimeFitness.ChallengeEngine.UI/js/Trainerlanding.js
@@ -1,46 +1,40 @@
 ﻿var app = angular.module('ltfAppTrainer', []);
 
-app.controller('CategoryCtrl', function ($scope, $http) {
-    $scope.Categorylist = [];
-    $http({
-        method: 'GET',
-        url: 'http://localhost:56507/api/Category/GetCategory'
-    }).success(function (result) {
-        console.log(result);
-        $scope.Categorylist = result;
-    });
-});
-
-app.controller('ChallengeCtrl', function ($scope, $http) {
-    $scope.Challengelist = [];
-    $http({
-        method: 'GET',
-        url: 'http://localhost:56507/api/Challenge/GetChallenges'
-    }).success(function (result) {
-        console.log(result);
-        $scope.Challengelist = result;
-    });
-});
-
 app.controller('myenroll', function ($scope, $http) {
     $scope.eCategorylist = [];
     $scope.eChallengelist = [];
     $scope.eUserlist = [];
+    $scope.eClublist = 0;
+    $http.get("http://localhost:56507/api/Club/GetClub").then(function (responses) {
+        console.log(responses);
+        $scope.eClublist = responses.data;
+        $scope.myClubValue = $scope.eClublist;
+    });
     $http.get("http://localhost:56507/api/Category/GetCategory").then(function (responses) {
         console.log(responses);
         $scope.eCategorylist = responses.data;
-        $scope.myCategoryValue = $scope.eCategorylist[0];
+        $scope.myCategoryValue = $scope.myCategoryValue;
     });
     $http.get("http://localhost:56507/api/Challenge/GetChallenges").then(function (responses) {
         console.log(responses);
         $scope.eChallengelist = responses.data;
-        $scope.myChallengeValue = $scope.eChallengelist[0];
+        $scope.myChallengeValue = $scope.myChallengeValue;
     });
     $http.get("http://localhost:56507/api/User/GetUsers").then(function (responses) {
         console.log(responses);
         $scope.eUserlist = responses.data;
         $scope.myUserValue = $scope.eUserlist[0];
     });
+    $scope.getCategory = function (myCategoryValue) {
+        console.log(myCategoryValue);
+        console.log(myeCategoryValue);
+        $scope.myeCategoryValue = myCategoryValue;
+    };
+    $scope.getChallenge = function (myChallengeValue) {
+        console.log(myCategoryValue);
+        console.log(myeChallengeValue);
+        $scope.myeChallengeValue = myChallengeValue;
+    };
     $scope.enrollUser = function (myCategoryValue, myChallengeValue, myUserValue) {
         var data = {
             category: myCategoryValue,
@@ -64,62 +58,3 @@ app.controller('myenroll', function ($scope, $http) {
         });
     };
 });
-
-//app.controller('UserCtrl', function ($scope, $http) {
-//    $scope.Userlist = [];
-//    $http({
-//        method: 'GET',
-//        url: 'http://localhost:56507/api/User/GetUsers'
-//    }).success(function (result) {
-//        console.log(result);
-//        $scope.Userlist = result;
-//    });
-//    var data = { UName: $scopeuserValue ,CName:$scope.    }
-//    $scope.enrollUser = function () {
-//        $http({
-//            method: 'POST',
-//            url: 'http://localhost:56507/api/Enrollment/PostEnrollment',
-
-//        });
-//    };
-//});
-
-
-
-
-
-//app.controller('ClubCtrl', function ($scope, $http) {
-//    $scope.clubslist = [];
-//    $http({
-//        method: 'GET',
-//        url: 'http://localhost:56507/api/Club/GetClub'
-//    }).success(function (result) {
-//        console.log(result);
-//        $scope.clubslist = result;
-//    });
-//});
-//app.controller("showCtrl", function ($scope, $location, myFactory) {
-//    $scope.Students = [
-//        { Name: "Akhilesh", Address: "Kolkata", Email: "xxxx@gmail.com" },
-//        { Name: "Mukesh", Address: "Delhi", Email: "yyyy@gmail.com" },
-//        { Name: "Rakesh", Address: "Mumbai", Email: "zzzz@gmail.com" },
-//    ]
-
-//    $scope.Edit = function (d) {
-//        myFactory.set(d);
-//        $location.path('/EditData');
-//    }
-//});
-
-//$http.get("http://localhost:56507/api/Club/GetClub").success(function (response) {
-//    var clubs = response.data;
-
-//    //$.map(data, function (clubs) {
-//    //    arrclubs.push(clubs.ClubName + "," + clubs.City);
-//    //});
-//    $scope.clubslist = arrclubs;
-//}).error(function (status) {
-//    alert(status);
-//});
-
-//});
